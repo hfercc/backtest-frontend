2063f6a5993dbc08a055f13aa36c2ff5211f8290
ChanYH <hfercxx@hotmail.com>
2018-08-30 20:20:02 +0800

    navbar dropdown: left -> right

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -17,12 +17,6 @@
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Dropdown
         </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
       </li>
       <li class="nav-item pull-right">
         <a class="nav-link disabled" href="#">Disabled</a>
@@ -39,7 +33,7 @@
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 {{username}}
               </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                 <a class="dropdown-item" href="javascript:;" data-toggle="modal" data-target="#changePassword">Change Password</a>
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="#" @click="logout()">Logout</a>
