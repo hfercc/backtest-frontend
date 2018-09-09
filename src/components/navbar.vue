9bc289e004ffa44ee4b122febef43cfe87844683
ChanYH <hfercxx@hotmail.com>
2018-09-09 11:16:30 +0800

    p

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -38,7 +38,7 @@
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="changePasswordModalCenterTitle">Modal title</h5>
+                    <h5 class="modal-title" id="changePasswordModalCenterTitle">Change Paassword</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
@@ -59,7 +59,7 @@
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary" @click="change_password">Submit</button>
+                    <button type="button" class="btn btn-custom" @click="change_password">Submit</button>
                 </div>
             </div>
         </div>
